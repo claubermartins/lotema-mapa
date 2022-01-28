563f4801ecff8e581c6c8aea65a7df7d0b624da1
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2022-01-28 17:08:02 -0300

    iniciando o projeto

---
+++ src/app/navbar-area/navbar-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-area',
+  templateUrl: './navbar-area.component.html',
+  styleUrls: ['./navbar-area.component.css']
+})
+export class NavbarAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
