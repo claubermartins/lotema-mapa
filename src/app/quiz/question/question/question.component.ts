a678a99b7c08a0ac779c2051f4d9993c42ea63d6
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2022-02-15 16:28:09 -0300

    inserindo botão sair no Quiz

--- src/app/quiz/question/question/question.component.ts
+++ src/app/quiz/question/question/question.component.ts
@@ -1,6 +1,7 @@
 import { QuestionService } from './../../service/question.service';
 import { Component, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-question',
@@ -13,13 +14,13 @@ export class QuestionComponent implements OnInit {
   public questionList: any = [];
   public currentQuestion: number = 0;
   public points: number = 0;
-  counter = 20;
+  counter = 40;
   correctAnswer: number = 0;
   inCorrectAnswer: number = 0;
   interval$: any;
   progress: string = "0";
   isQuizCompleted : boolean = false;
-  constructor(private questionService: QuestionService) { }
+  constructor(private questionService: QuestionService, private router:Router) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
@@ -71,13 +72,13 @@ export class QuestionComponent implements OnInit {
         this.counter--;
         if (this.counter === 0) {
           this.currentQuestion++;
-          this.counter = 20;
+          this.counter = 40;
           this.points -= 10;
         }
       });
     setTimeout(() => {
       this.interval$.unsubscribe();
-    }, 200000);
+    }, 400000);
   }
   stopCounter() {
     this.interval$.unsubscribe();
@@ -85,14 +86,14 @@ export class QuestionComponent implements OnInit {
   }
   resetCounter() {
     this.stopCounter();
-    this.counter = 20;
+    this.counter = 40;
     this.startCounter();
   }
   resetQuiz() {
     this.resetCounter();
     this.getAllQuestions();
     this.points = 0;
-    this.counter = 20;
+    this.counter = 40;
     this.currentQuestion = 0;
     this.progress = "0";
 
@@ -100,6 +101,12 @@ export class QuestionComponent implements OnInit {
   getProgressPercent() {
     this.progress = ((this.currentQuestion / this.questionList.length) * 100).toString();
     return this.progress;
+
+  }
+  sairQuiz(){
+    setTimeout(() => {
+      this.router.navigate(['/navbar-area'])
+    }, 500);
 
   }
 }
